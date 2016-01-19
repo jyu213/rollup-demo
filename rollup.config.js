174211b99e3a7e2cce52131189c575cb9ff299b7
j4evee <jyu213@gmail.com>
2016-01-19 13:24:55 +0800

    modify: package

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,19 @@
 var babel = require('rollup-plugin-babel');
 var uglify = require('rollup-plugin-uglify');
 var npm = require('rollup-plugin-npm');
+var commonjs = require('rollup-plugin-commonjs');
 
 export default{
     entry: 'src/index.js',
-    format: 'umd',
     external: ['moment'],
+    format: 'umd',
     moduleName: 'DatePick',
     sourceMap: true,
     dest: 'index.js',
     plugins: [
-        // npm({jsnext: true, main: true}),
-        // uglify(),
+        npm({ jsnext: true, main: true }),
+        commonjs(),
+        uglify(),
         babel({
             exclude: 'node_modules/**',
             presets: [ "es2015-rollup" ]
